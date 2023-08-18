a3318fbd29e84038a603a8a58110b7b37830d2bb
Lukas Stratmann <stratmann@ccs-labs.org>
2023-08-18 15:56:15 +0200

    Add vce-container definition
    Since this repository is public now, we don't need a separate
    vce-container repository anymore.

---
+++ scripts/build-container-image.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+sudo apptainer build $SCRIPT_DIR/../vce-container.sif $SCRIPT_DIR/../vce-container.def
